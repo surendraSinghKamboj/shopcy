756aef02540567fa2490240b059d1161cd8d8635
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-03 16:29:38 +0530

    margin changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <header class="flex flex-col md:flex-row md:justify-between items-center bg-lime-500 py-2">
       <div class="w-full md:w-auto">
-        <h1 class="m-2 text-lg text-white cursor-pointer hover:text-teal-300 md:w-auto md:mx-0">
+        <h1 class="m-2 mx-2 text-lg text-white cursor-pointer hover:text-teal-300 md:w-auto md:mx-0">
           Shopcy
         </h1>
       </div>
       <nav class="w-full md:w-auto">
-        <ul class="flex justify-center md:justify-end">
+        <ul class="flex justify-center md:justify-end mx-2">
           <li class="m-2 mx-3 hover:bg-white hover:text-lime-500 p-1 cursor-pointer">
             Homepage
           </li>
