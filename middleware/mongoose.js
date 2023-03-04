136ce20f4608c8404ea4ee04140c194f2feac886
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-04 18:32:02 +0530

    env setup

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -6,7 +6,10 @@ const connectDb = (handler) => async (req, res) => {
     if (mongoose.connections[0].readyState) {
         return handler(req, res)
     }
-    await mongoose.connect("mongodb://0.0.0.0:27017/shopcy")
+    if(!process.env.MONGODB_URI){
+        console.error("URI Not found")
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected Successfully....")
 }
 
