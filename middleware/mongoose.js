a07e22481cc848da75e60b9557c7211fe48d9dca
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-04 15:43:32 +0530

    Update mongoose Env

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -6,7 +6,7 @@ const connectDb = (handler) => async (req, res) => {
     if (mongoose.connections[0].readyState) {
         return handler(req, res)
     }
-    await mongoose.connect("mongodb+srv://vercel-admin-user:Java@8164@cluster0.8doc4qh.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("Database connected Successfully....")
 }
 
