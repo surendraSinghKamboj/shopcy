d4cd56bc75acb3de6596e265754cc0aefa966ab7
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-04 08:34:15 +0530

    Schemas Created

---
+++ models/products.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Products = new mongoose.Schema({
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    imgs: [{ type: String }],
+    category: { type: String, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number },
+    quantity: { type: Number, required: true }
+})
+
+
+export default mongoose.model("products", Products);
