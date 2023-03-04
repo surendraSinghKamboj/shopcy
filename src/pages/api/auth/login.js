0e5ac671a41bdaf4d2b121c67f2e74b8fdad9384
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-05 01:24:30 +0530

    login & Signup api created

---
+++ src/pages/api/auth/login.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import user from "../../../../models/user";
+import connectDb from "../../../../middleware/mongoose";
+
+const handler = async (req, res) => {
+    if (req.method === "POST") {
+        try {
+            const { userName, password } = req.body;
+
+            const existingUser = await user.findOne({ userName });
+
+            if (!existingUser) {
+                return res.status(401).json({ message: "Invalid username or password" });
+            }
+
+            
+            const passwordMatch = await bcrypt.compare(password, existingUser.password);
+
+            if (!passwordMatch) {
+                return res.status(401).json({ message: "Invalid username or password" });
+            }
+
+            const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+            return res.status(200).json({ message: "Login successful", token });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    } else {
+        res.status(400).json({ message: "Bad Request. Please try again." });
+    }
+};
+
+export default connectDb(handler);
---
+++ src/pages/api/auth/signup.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import user from "../../../../models/user";
+import connectDb from "../../../../middleware/mongoose";
+
+const handler = async (req, res) => {
+    if (req.method === "POST") {
+        try {
+            const salt = await bcrypt.genSalt(10);
+
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+            let u = new user({
+                name: req.body.name,
+                userName: req.body.userName,
+                email: req.body.email,
+                mobile: req.body.mobile,
+                password: hashedPassword,
+            });
+            await u.save();
+
+            res.status(200).json({
+                message: `Congratulations ${req.body.name}! Your account has been created successfully.`,
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    } else {
+        res.status(400).json({ message: "Bad Request. Please try again." });
+    }
+};
+
+export default connectDb(handler);
