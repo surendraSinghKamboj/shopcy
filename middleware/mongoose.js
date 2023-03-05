91e54222892b3294f3feef8c2844ffefe895ef5a
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-05 08:01:38 +0530

    mongoose connection

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 
 
-
 const connectDb = (handler) => async (req, res) => {
-    if (mongoose.connections[0].readyState) {
-        return handler(req, res)
+    try {
+        if (mongoose.connections[0].readyState) {
+            return handler(req, res)
+        }
+        if (!process.env.MONGODB_URI) {
+            console.error("URI Not found")
+        }
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.info("Online Database connected Successfully...........")
+    } catch (error) {
+        console.error("Failed to connecting online Database Plaese Check your Internet Connection.....", error);
+    } finally {
+        await mongoose.connect(process.env.LOCAL_MONGODB_URI);
+        console.info("local Database connected..............")
     }
-    if(!process.env.MONGODB_URI){
-        console.error("URI Not found")
-    }
-    await mongoose.connect(process.env.MONGODB_URI);
-    
-    console.log("Database connected Successfully....")
+
 }
 
 export default connectDb;
